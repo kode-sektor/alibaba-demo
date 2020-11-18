6a4ef046edd50fa295120ab1e5650ddf14b91f5a
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-11-18 10:19:47 -0500

    Add styling for Order page (2)

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL : 'http://localhost:5001/challenge-18b20/us-central1/api'  // This is gotten after running 'firebase emulator:start' in console
+    baseURL: 'http://localhost:5001/challenge-18b20/us-central1/api'  // This is gotten after running 'firebase emulator:start' in console
+    // When it's time for live deployment, you'll substitute this local url with that found in the Firebase functions, something like: 
+    // https://us-central1-challenge-4b2b2.cloudfunctions.net/api
 })
 
 export default instance
+
